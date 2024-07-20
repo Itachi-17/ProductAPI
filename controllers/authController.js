56704877a602d3daf0385fe59cdffdd9d344860d
Rohit Rathod <rathodrohit2121@gmail.com>
2024-07-20 23:10:04 +0530

    some changes made in controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,6 +11,11 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please provide all required fields.");
   }
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists.");
+  }
   const hashedPassword = await bcrypt.hash(password, 10);
   const user = await User.create({
     name,
